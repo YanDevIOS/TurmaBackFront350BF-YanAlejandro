75128e84687d279411ea296b233df8e14c24f483
Yan Alejandro <61808807+YanDevIOS@users.noreply.github.com>
2023-03-07 20:47:58 -0300

    atividade 1: criando três classes com cinco atributos e dois métodos

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -26,3 +26,51 @@ class Automovel {
         print("O carro está ligado")
     }
 }
+
+class Pessoa {
+    var nome: String = "Yan"
+    var cor: String = "Pardo"
+    var sexo: String = "Masculino"
+    var altura: Float = 1.84
+    var casado: Bool = true
+    
+    func codar() {
+        print("O Yan está codando")
+    }
+    
+    func falar() {
+        print("O Yan está falando")
+    }
+}
+
+class Gorila {
+    var mamifero: Bool = true
+    var peso: Float = 200.0
+    var cor: String = "Preto"
+    var altura: Float = 1.75
+    var bipede: Bool = true
+    
+    func correr() {
+        print("o animal está correndo")
+    }
+    
+    func morder() {
+        print("O Animal está mordendo")
+    }
+}
+
+class Aviao {
+    var modelo: String = "airbus798"
+    var peso: Float = 500000.00
+    var cor: String = "Branco"
+    var comprimento: Int = 80
+    var bipede: Bool = true
+    
+    func voar() {
+        print("O avião está voando")
+    }
+    
+    func acelerar() {
+        print("O Avião está acelerando")
+    }
+}
