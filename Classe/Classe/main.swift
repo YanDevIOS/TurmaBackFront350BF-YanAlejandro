a5c551f10cc6d8db2edaf0a8a90375d51146abaf
Yan Alejandro <61808807+YanDevIOS@users.noreply.github.com>
2023-03-07 20:26:38 -0300

    primeira classe criada

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -7,5 +7,22 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK: -  OBJETO
+//CARACTERISTICA DE UM OBJETO -> ATRIBUTOS || PROPRIEDADES
+//AÇÕES -> FUNÇÃO / METODO
 
+class Automovel {
+    var color: String = "Preto"
+    var doors: Int = 4
+    var mark: String = "Maserati"
+    var isEletric: Bool = false
+    var peapleComport: Int = 4
+    
+    func ligarFarol() {
+        print("O farol está ligado")
+    }
+    
+    func ligarOCarro() {
+        print("O carro está ligado")
+    }
+}
